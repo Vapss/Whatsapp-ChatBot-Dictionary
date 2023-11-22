f30be6eb0d19f9fc729ef25ad5d4111832daa5b0
José Antonio Vázquez Portuguez <josvapstg@gmail.com>
2023-11-21 19:07:16 -0600

    First Push
    First Push

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Form
+import requests
+from utils import send_message
+from dotenv import load_dotenv
+import os
+from typing import List
+from models import dictionary_collection
+import urllib.parse
+
+load_dotenv()
+
+app = FastAPI()
+whatsapp_number = os.getenv("TO_NUMBER")
+api_key = os.getenv("DICTIONARY_API_KEY")
+
+@app.post("/message")
+async def reply(Body: str = Form()):
+    escaped_body = urllib.parse.quote(Body)
+    url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{escaped_body}?key={api_key}"
+    flag = "Please give a valid word"
+
+    if Body.isalpha():
+        response = requests.get(url)
+        print("Response:", response)
+        data = response.json()
+
+        definition = data[0]["shortdef"][0]
+
+        send_message(whatsapp_number, definition)
+    else:
+        return send_message(whatsapp_number, flag)
+
+    return ""
